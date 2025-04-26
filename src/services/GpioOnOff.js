cfed45e610c946aaf494fac2261ad739e88a5693
tm <vlad3dracula@gmail.com>
2025-04-26 08:22:24 +0200

    added  onOff gpio class

---
+++ src/services/GpioOnOff.js
@@ -0,0 +1,25 @@
+import { convertPinState } from "../helpers/pinHelper.js"
+import PinState from "../models/PinState.js"
+import { Gpio } from 'onoff'
+
+export default class GpioONoFF {
+    #pinNo
+    #gpio
+
+    constructor(pinNo, connect = false) { 
+        this.#pinNo = pinNo
+        this.#gpio = new Gpio.Gpio(pinNo, 'out')
+        this.setState(PinState.LOW)
+    }
+
+    setState = (state) => {
+        this.#gpio.writeSync(convertPinState(state))
+    }
+    getState = () => convertPinState(this.#gpio.readSync())
+    getPinNo = () => this.#pinNo
+    init = () => {}
+    destroy = () => {
+        this.#gpio.unexport()
+        this.#pinNo = null
+    }
+}
